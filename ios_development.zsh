0e015d56281a7fd4c6dba594f9a52038ba81c575
Gio <giovanni.lodi42@gmail.com>
2013-11-25 22:24:45 +0000

    Added aliases for ios development

---
+++ ios_development.zsh
@@ -0,0 +1,5 @@
+alias xco='open -a Xcode .'
+alias pi='nocorrect pod install && xco'
+# Note! this assumes you have xctool available in your path
+# and the .xctoolargs in the current working directory
+alias xt='xctool test'
