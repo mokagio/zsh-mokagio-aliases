0177e619df872be8ce03dad2c75f52d1bfb08918
Gio <giovanni.lodi42@gmail.com>
2013-12-02 09:47:57 +0000

    Added alias for simple pod install

--- ios_development.zsh
+++ ios_development.zsh
@@ -1,4 +1,5 @@
 alias xco='open -a Xcode .'
+alias pii='nocorrect pod install'
 alias pi='nocorrect pod install && xco'
 # Note! this assumes you have xctool available in your path
 # and the .xctoolargs in the current working directory
