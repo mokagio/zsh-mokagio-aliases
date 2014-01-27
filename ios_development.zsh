6f84c026508472832ddb3444ddfbf1be438c3c47
Gio <giovanni.lodi42@gmail.com>
2014-01-27 11:02:22 +0000

    Added nocorrect alias for fui gem command

--- ios_development.zsh
+++ ios_development.zsh
@@ -1,7 +1,14 @@
 alias xco='open -a Xcode .'
+
 alias aco='open -a AppCode .'
+
 alias pii='nocorrect pod install'
 alias pi='nocorrect pod install && xco'
+
+# Ruby gem to find unused Objective-C imports, and eventually delete them
+# https://github.com/dblock/fui
+alias fui='nocorrect fui'
+
 # Note! this assumes you have xctool available in your path
 # and the .xctoolargs in the current working directory
 alias xt='xctool test'
